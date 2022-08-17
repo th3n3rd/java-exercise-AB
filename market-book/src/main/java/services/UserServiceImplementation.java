61187d7c55517a394c2f35191a8fdf285081805c
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-08-17 16:06:49 +0100

    Refactor out common code to hash (that is not encryption) a password

--- market-book/src/main/java/services/UserServiceImplementation.java
+++ market-book/src/main/java/services/UserServiceImplementation.java
@@ -15,33 +15,15 @@ public class UserServiceImplementation implements UserServiceInterface {
 
     @Override
     public User registerUser(User user) {
-        /* Plain-text password initialisation. */
-        var plaintextPassword = user.getUserPassword();
         try {
-            /* MessageDigest instance for MD5. */
-            var messageDigest = MessageDigest.getInstance("MD5");
-
-            /* Add plain-text password bytes to digest using MD5 update() method. */
-            messageDigest.update(plaintextPassword.getBytes());
-
-            /* Convert the hash value into bytes */
-            var bytes = messageDigest.digest();
-
-            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */
-            var hashedPassword = new StringBuilder();
-            for (var i = 0; i < bytes.length; i++) {
-                hashedPassword.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            /* Complete hashed password in hexadecimal format */
-            //create the new object with the new encrypted paswword
-            var usr = new User(
+            // create the new object with the new hashed paswword
+            var newUser = new User(
                 user.getUsername(),
                 user.getUserSurname(),
                 user.getUserEmail(),
-                hashedPassword.toString()
+                hashPassword(user.getUserPassword())
             );
-
-            return this.userPersistence.registerUser(usr);
+            return this.userPersistence.registerUser(newUser);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
@@ -51,25 +33,30 @@ public class UserServiceImplementation implements UserServiceInterface {
     @Override
     public String loginUser(String email, String password) {
         try {
-            /* MessageDigest instance for MD5. */
-            var messageDigest = MessageDigest.getInstance("MD5");
-
-            /* Add plain-text password bytes to digest using MD5 update() method. */
-            messageDigest.update(password.getBytes());
-
-            /* Convert the hash value into bytes */
-            var bytes = messageDigest.digest();
-
-            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */
-            var hashedPassword = new StringBuilder();
-            for (var i = 0; i < bytes.length; i++) {
-                hashedPassword.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            /* Complete hashed password in hexadecimal format */
-            return this.userPersistence.loginUser(email, hashedPassword.toString());
+            return this.userPersistence.loginUser(email, hashPassword(password));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private static String hashPassword(String plaintextPassword) throws NoSuchAlgorithmException {
+        /* MessageDigest instance for MD5. */
+        var messageDigest = MessageDigest.getInstance("MD5");
+
+        /* Add plain-text password bytes to digest using MD5 update() method. */
+        messageDigest.update(plaintextPassword.getBytes());
+
+        /* Convert the hash value into bytes */
+        var bytes = messageDigest.digest();
+
+        /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */
+        var hashedPassword = new StringBuilder();
+        for (var i = 0; i < bytes.length; i++) {
+            hashedPassword.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        /* Complete hashed password in hexadecimal format */
+        return hashedPassword.toString();
     }
 }
